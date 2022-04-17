d68ac7f737b1f0a809d32e51d5b92b3b486519c7
Gimena <gimena.bugiolachio@gmail.com>
2022-04-17 00:09:51 -0300

    Agregamos el mock-task y el Task

---
+++ src/app/mock-task.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export  const TASKS:Task[]  = [
+    {
+        id: 1,
+        text: "Terminar primer módulo de Angular",
+        day: 'Abril 29 a las 17:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Hacer el segundo módulo de Angular",
+        day: 'Mayo 13 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Moquetar Portfolio en Angular",
+        day: 'Julio 15 a las 00:00',
+        reminder: true,
+    },
+]
